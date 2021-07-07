8e074452db0d281130a87e2885c8ddf4780bbb92
infinnitor <infinnitor@gmail.com>
2021-07-07 03:31:29 +0100

    Add targeting and angle system to bullets

--- Source/main.py
+++ Source/main.py
@@ -14,7 +14,7 @@ class player_class(sprite):
         self.default_speed = speed
         self.focus_speed = self.default_speed // 2
 
-        self.offset = u.offset_point(self, (0, self.r * -1))
+        self.bullet_offset = u.offset_point(self, (0, self.r * -1)))
 
         self.sprites = sprites
 
@@ -50,30 +50,47 @@ class player_class(sprite):
             self.y = oldy
 
         if game.check_key(pygame.K_SPACE, timebuffer=7):
-            game.add_sprite(player_bullet(
-                                    pos=self.offset.get_pos(),
+            game.add_sprite(bullet(
+                                    pos=self.bullet_offset.get_pos(),
                                     radius=self.r//2,
                                     speed=10,
+                                    angle=-90,
                                     sprites=None))
 
     def update_draw(self, game):
         pygame.draw.circle(game.win, game.colours.red, (self.x, self.y), self.r)
 
-class player_bullet(sprite):
-    def __init__(self, pos, radius, speed, sprites):
+class bullet(sprite):
+    def __init__(self, pos, radius, speed, angle, sprites, target=None):
         self.x = pos[0]
         self.y = pos[1]
         self.r = radius
 
         self.speed = speed
+        self.angle = angle
+
+        a = math.radians(self.angle)
+        self.xmove = math.cos(a)
+        self.ymove = math.sin(a)
+
+        self.target = target
 
         self.sprites = sprites
 
     def update_move(self, game):
         if self.onscreen(game):
-            self.y -= self.speed
+            self.x += self.xmove * self.speed
+            self.y += self.ymove * self.speed
         else:
             self.destroy = True
+
+        if self.target is not None:
+            a = math.atan2(self.target.x - self.x, self.target.y - self.y)
+            self.xmove = math.cos(a)
+            self.ymove = math.sin(a)
+
+            if u.circle_collide(self, self.target):
+                self.destroy = True
 
     def update_draw(self, game):
         pygame.draw.circle(game.win, game.colours.blue, (self.x, self.y), self.r)
