ae7d5b985cd87efbd481116567d212ac97364e3f
Infinnitor <43831721+Infinnitor@users.noreply.github.com>
2021-07-14 18:16:47 +0100

    Create draw_utils and add functions

---
+++ Source/draw_utils.py
@@ -0,0 +1,41 @@
+import random
+
+
+def fix_rgb(colour, min=1, max=255):
+    assert min > 0 and max < 256, "Min and Max must be between 1 and 255"
+
+    fixed_colour = []
+    for c in colour:
+        if c > max:
+            c = max
+        elif c < min:
+            c = min
+
+        fixed_colour.append(c)
+
+    return tuple(fixed_colour)
+
+
+def randomize_rgb(colour, upper=-20, lower=20):
+    final_colour = [c + random.randint(upper, lower) for c in colour]
+    return fix_rgb(final_colour)
+
+
+class particle_functions():
+    def explosion(number, pos, speed, colour, game, lifetime=30, randcol=False, layer="HIGH"):
+        for p in range(number):
+
+            final_colour = colour
+            if randcol is True:
+                final_colour = randomize_rgb(colour)
+
+            new_part = game.particle(
+                                    pos=pos,
+                                    size=15,
+                                    speed=speed,
+                                    angle=random.randint(0, 359),
+                                    lifetime=lifetime,
+                                    colour=final_colour)
+
+            new_part.name = layer + "PARTICLE"
+            game.add_sprite(new_part)
